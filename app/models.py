a8abae1074248ad227dac1dd11c9c5df2004912b
PWX <306235911@qq.com>
2016-08-29 17:20:33 +0800

    upload
    uploading

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # -*- coding:utf8 -*-
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask.ext.login import UserMixin
+from . import login_manager
 
 
 class Item(db.Model):
@@ -31,9 +33,10 @@ class Role(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64) , unique=True , index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
     def password(self):
@@ -48,3 +51,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
